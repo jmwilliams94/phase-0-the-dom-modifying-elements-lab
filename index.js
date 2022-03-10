8bceaabf5117b02338cc42004e78e9e72adf5a48
Jordan <jmwilliams94@icloud.com>
2022-03-10 19:54:12 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = 'Jordan is the champion!';
